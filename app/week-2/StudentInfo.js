54584ca81f9871a81ebe799c7875dc9fcbb143d3
Kanwar0135 <anmol.kanwar1@edu.sait.ca>
2026-01-22 13:12:22 -0700

    edit

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Anmol Kanwar</h1>
+
+      <p>
+        <Link href="">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
